d91c975245f2510d145f7a2c83e46f1a22bd46f9
Jonathan Bispo <jonathan26062001@hotmail.com>
2021-02-04 05:11:51 -0300

    Segundo dia

--- src/spyders/spyder.py
+++ src/spyders/spyder.py
@@ -1,31 +1,60 @@
 import scrapy
-import json
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-conteudo = {}
-
-
-class Spyder(scrapy.Spider):
-    name = 'main-spyder'
-    start_urls = ['https://financenews.com.br/2021/02/copel-sofre-ataque-cibernetico/']
+class Spyder(CrawlSpider):
+    name = 'finance-news'
+    start_urls = ['https://financenews.com.br']
     allowed_domains = ['financenews.com.br']
 
+    custom_settings = {
+        'DEPTH_LIMIT': '7',
+        'FEED_EXPORT_ENCODING': 'utf-8'
+    }
 
-    def parse(self, response):
-        titulo = response.xpath('//h1/text()').get()
+    rules = (
+        Rule(
+            LinkExtractor(restrict_xpaths=(
+                "//nav//a[contains(text(),'Não deixe de ler')]")
+            )
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_css=(".nextpostslink")
+            )
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_css=(".archive-item")
+            ),
+            callback='parse_noticia'
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_css=(".mais-lidas")
+            ),
+            callback='parse_noticia'
+        )
 
-        texto = ""
-        for paragrafo in response.xpath('//div/p/span/text()').getall():
-            texto += paragrafo.replace(u'\xa0', " ")
+    )
 
-        if titulo and len(texto) > 300:
-            conteudo[titulo.strip()] = texto
-        del texto, titulo
+    def parse_noticia(self, response):
+        article = " ".join(response.css('.page-post').xpath("//p[not(contains(.//text(), 'aqui')" +
+                                                  " or contains(.//text(), 'Leia mais sobre')" +
+                                                  " or contains(.//text(), 'o tema de compras coletivas de ações')" +
+                                                  " or contains(.//text(), 'Para receber notícias')" +
+                                                  " or contains(.//text(), 'hatsapp')" +
+                                                  " or contains(.//text(), 'entre nesse grupo')" +
+                                                  " or contains(.//text(), 'Publicado às')" +
+                                                  " or contains(.//text(), 'Atualizado às')" +
+                                                  " or contains(.//text(), 'elegram')" +
+                                                  " or contains(.//text(), 'Finance News')" +
+                                                  " or contains(.//text(), 'clique')" +
+                                                  " or contains(.//text(), 'leia)" +
+                                                  " or contains(.//text(), 't.me/'))]//text()").getall()).replace(
+            '\xa0', '')
 
-        for next_page in response.css('a'):
-            if len(conteudo) < 2:
-                yield response.follow(next_page, self.parse)
-            else:
-                with open('../data/titulo_conteudo.json', 'w', encoding='utf8') as outfile:
-                    json.dump(conteudo, outfile, ensure_ascii=False)
-
+        if len(article) > 199:
+            yield {response.url: article.strip()}
