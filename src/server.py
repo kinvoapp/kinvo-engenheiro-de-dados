a8e652da5043e477d16c8b2e20636a89985352ba
Jonathan Bispo <jonathan26062001@hotmail.com>
2021-02-04 21:32:01 -0300

    Falta API

--- src/server.py
+++ src/server.py
@@ -1,15 +1,38 @@
 from flask import Flask
 
+import spacy
+import json
+
+
+from scrapy.crawler import CrawlerProcess
+import os
+
+
+import spider.spider as spider
+
+nlp = spacy.load('pt_core_news_sm')
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return open('./web/index.html').read().replace("msg",'/minerar para minerar<br>/extrair para extrair')
 
+
 @app.route('/minerar')
 def minerarSalvar():
-    return open('./web/index.html').read().replace("msg",'Minerando...')
+    return ''
 
 @app.route('/extrair')
 def extrairEntidades():
-    return open('./web/index.html').read().replace("msg",'Extraindo...')
+    with open('data/dados.json', 'r', encoding='utf-8') as news_file:
+        news = json.loads(news_file.read())[0:5]
+    output = {}
+    for new in news:
+        new = new['news']
+        doc = nlp(new)
+        entidades = []
+        for entity in doc.ents:
+            entidades.append({entity.text: entity.label_})
+        output[new] = entidades
+    return output
---
+++ src/spider/spider.py
@@ -0,0 +1,77 @@
+import scrapy
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+import json
+with open('data/empresas_B3.json','r') as b3_file:
+    b3=json.load(b3_file)
+with open('data/words.json','r') as words_file:
+    words=json.load(words_file)
+
+
+def contain(string, arr, value):
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] in string:
+            count +=1
+            if count == value:
+                return True
+    return False
+
+
+class Spyder(CrawlSpider):
+    name = 'finance-news'
+    start_urls = ['https://financenews.com.br']
+    allowed_domains = ['financenews.com.br']
+
+    custom_settings = {
+        'DEPTH_LIMIT': '8',
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'FEED_URI': 'data/dados.json'
+    }
+
+    rules = (
+        Rule(
+            LinkExtractor(restrict_xpaths=(
+                "//nav//a[contains(text(),'Não deixe de ler')]")
+            )
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_css=(".nextpostslink")
+            )
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_css=(".archive-item")
+            ),
+            callback='parse_noticia'
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_css=(".mais-lidas")
+            ),
+            callback='parse_noticia'
+        )
+
+    )
+
+    def parse_noticia(self, response):
+        paragrafos = []
+
+        data = response.css('.pull-left').xpath('.//text()').get().strip()
+
+        for paragrafo in response.css('.page-post').xpath("//p/span/text()").getall():
+            paragrafo = paragrafo.replace('\xa0', '')
+            paragrafo = paragrafo.strip()
+
+            if len(paragrafo) > 50 and contain(paragrafo, b3, 1) and contain(paragrafo, b3, 2):
+                yield {'news': data + " " + paragrafo}
+
+        for paragrafo in response.css('.page-post').xpath("//p/text()").getall():
+            paragrafo = paragrafo.replace('\xa0', '')
+            paragrafo = paragrafo.strip()
+
+            if len(paragrafo) > 50 and contain(paragrafo, b3, 1) and contain(paragrafo.lower(), words, 2):
+                yield {'news': data + " " + paragrafo}
