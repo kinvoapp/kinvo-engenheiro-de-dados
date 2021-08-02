e08610baafe1c90c59396d00396eec335c44a654
Beatrizdacruz <83562801+Beatrizdacruz@users.noreply.github.com>
2021-08-02 10:57:15 -0300

    content saved in news.csv file.

--- notice.py
+++ notice.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-site = requests.get("https://financenews.com.br/").content
+site = requests.get("https://financenews.com.br").content
 # objetos site recebe o conteúdo da requisição.
 
 soup = BeautifulSoup(site, 'html.parser')
 # soup baixa do site o html.
 
 news = soup.find_all('div', class_="manchete manchete3 dark")
-print(soup.title.string)
-print(news)
+
+
+with open('news.csv', 'a', newline='', encoding='UTF-8') as f:
+    print(soup.title.string)
+    print(news)
