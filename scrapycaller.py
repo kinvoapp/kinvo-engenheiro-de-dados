8a47278782d8f4a39f8febb5eb71869a3ce9e976
gsimoes00 <gustavomota001@gmail.com>
2021-08-23 20:46:02 -0300

    Classe ScrapyCaller para chamar crawler adicionada

---
+++ scrapycaller.py
@@ -0,0 +1,14 @@
+import os
+from subprocess import check_output
+
+class ScrapyCaller(object):
+
+    scrapy_folder = 'news_scraper'
+    call_string = 'scrapy crawl finance-news --nolog --output -:json'
+
+    def __init__(self):
+        self.working_dir = os.path.join(os.path.dirname(__file__), self.scrapy_folder, '')
+        print(self.working_dir)
+
+    def get_scrapy_output(self):
+        return check_output(self.call_string.split(), cwd=self.working_dir).decode('unicode_escape')
