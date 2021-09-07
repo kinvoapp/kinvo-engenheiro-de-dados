c66517d452ba1bfffe36e8d9c06e3a9ebf74f8b5
João Paulo Canário <jopacanario@gmail.com>
2021-09-07 15:51:11 -0300

    Decrease download delay to 3 seconds

--- kinvo/core/config.py
+++ kinvo/core/config.py
@@ -10,7 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 DEPTH_LIMIT = 10
 DOWNLOAD_TIMEOUT = 540
-DOWNLOAD_DELAY = 5
+DOWNLOAD_DELAY = 3
 
 EXTENSIONS = {
     'scrapy.extensions.telnet.TelnetConsole': None,
@@ -18,6 +18,7 @@ EXTENSIONS = {
 }
 
 FEED_EXPORT_ENCODING = 'utf-8'
+
 NEWSPIDER_MODULE = 'kinvo.services'
 ROBOTSTXT_OBEY = True
 SPIDER_MODULES = ['kinvo.services']
