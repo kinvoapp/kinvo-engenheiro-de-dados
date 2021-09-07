4901696f314b452dd3d1fafca9b30aeb0c3f8a68
João Paulo Canário <jopacanario@gmail.com>
2021-09-07 14:17:33 -0300

    Parse content of ultimoinstante.com.br

--- kinvo/services/crawler.py
+++ kinvo/services/crawler.py
@@ -1,5 +1,7 @@
 import re
+import html
 import scrapy
+import w3lib.html
 
 from kinvo.logger import logger
 
@@ -7,14 +9,41 @@ from kinvo.logger import logger
 class FinanceSpider(scrapy.Spider):
     name = 'financenews'
     allowed_domains = ['financenews.com.br']
-    start_urls = ["https://financenews.com.br/feed/"]
+    start_urls = ["https://financenews.com.br/feed/",
+                  "https://ultimoinstante.com.br/feed/"]
 
     def parse(self, response):
+        if 'financenews.com.br' in response.url:
+            self.parse_finance(response)
+
+    def parse_instant(self, response):
+        stocks_kw = ['ibovespa', 'ações', 'B3']
+
+        logger.info(f"Parsing {response.url}")
+
+        for item in response.css('item'):
+            categories = item.css('category::text').getall()
+
+            has_keywords = any(kw in categories for kw in stocks_kw)
+
+            if has_keywords:
+                title = item.css('title::text').get()
+                pub_date = item.css('pubDate::text').get()
+                link = item.css('link::text').get()
+
+                content = item.css('description::text').get()
+                content = w3lib.html(content).replace('\n', ' ')
+                content = html.unescape(content.strip())
+
+                yield {'title': title, 'link': link,
+                       "pub_date": pub_date, 'content': content}
+
+    def parse_finance(self, response):
         stock_regex = r"[A-Za-z]{4}\d{1,2}"
         stocks_kw = ['ibovespa', 'ações', 'B3']
         reject_kw = ['notícias corporativas']
 
-        logger.info("Parsing page")
+        logger.info(f"Parsing {response.url}")
 
         for item in response.css('item'):
             url = item.css('link::text').get()
@@ -30,9 +59,9 @@ class FinanceSpider(scrapy.Spider):
                          f"has_rejects: {has_rejects}"))
 
             if not has_rejects and has_keywords:
-                yield scrapy.Request(url=url, callback=self.load_news)
+                yield scrapy.Request(url=url, callback=self.load_finance)
 
-    def load_news(self, response):
+    def load_finance(self, response):
         title = response.css('title::text').get().strip()
         link = response.url
         pub_date = response.css('.pull-left::text').get().strip()
@@ -43,8 +72,8 @@ class FinanceSpider(scrapy.Spider):
         if content:
             logger.info("Loaded News:")
             logger.info((f"Title: {title}, "
-                        f"link: {link}, "
-                         f"Publish on: {pub_date}"))
+                         f"link: {link}, "
+                         f"Published on: {pub_date}"))
 
             yield {'title': title, 'link': link,
                    "pub_date": pub_date, 'content': content}
